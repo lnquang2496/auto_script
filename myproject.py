3aad61bcc760b29032688e17a1ab8e31cc5e8e97
quangla <lnquang2496@gmail.com>
2019-10-22 22:02:08 +0700

    first commit

---
+++ myproject.py
@@ -0,0 +1,109 @@
+from openpyxl import load_workbook
+
+# Load worksheet
+def load_worksheet(filename, sheetname):
+	wb = load_workbook(filename)
+	ws = wb[sheetname]
+	return ws
+
+# Find cell boundary
+def find_cell(ws, target, match_case=False, find_all=False):
+	return_value = list()
+	target_found = False
+	# If target is string type
+	if isinstance(target, str):
+		for rows in ws:
+			for cell in rows:
+				if match_case:
+					target_found = target in cell.value
+				else:
+					target_found = target == cell.value
+
+				if target_found:
+					not_merged_cell = True
+					for merged_cell in ws.merged_cells:
+						if (cell.coordinate in merged_cell):
+							temp = {'firstcol':merged_cell.bounds[0],
+									'firstrow':merged_cell.bounds[1],
+									'lastcol':merged_cell.bounds[2],
+									'lastrow':merged_cell.bounds[3]}
+							not_merged_cell = False
+							if find_all:
+								return_value.append(temp)
+							else:
+								return temp
+					if not_merged_cell:
+						temp = {'firstcol':cell.column,
+								'firstrow':cell.row,
+								'lastcol':cell.column,
+								'lastrow':cell.row}
+						if find_all:
+							return_value.append(temp)
+						else:
+							return temp
+	# If target is list type, list format [column, row]
+	elif isinstance(target, list):
+		# list  = [col, row]
+		coor = ws.cell(column=target[0], row=target[1]).coordinate
+		not_merged_cell = True
+		for merged_cell in ws.merged_cells:
+			if (coor in merged_cell):
+				temp = {'firstcol':merged_cell.bounds[0],
+						'firstrow':merged_cell.bounds[1],
+						'lastcol':merged_cell.bounds[2],
+						'lastrow':merged_cell.bounds[3]}
+				not_merged_cell = False
+				return temp
+		if not_merged_cell:
+			temp = {'firstcol':target[0],
+					'firstrow':target[1],
+					'lastcol':target[0],
+					'lastrow':target[1]}
+			return temp
+
+	return return_value
+
+# Get cell value with cell coordinate
+def get_cell_value(ws, coor):
+	for row in range (coor['firstrow'], coor['lastrow'] + 1):
+		for col in range (coor['firstcol'], coor['lastcol'] + 1):
+			val = ws.cell(column=col, row=row).value
+			if (val != None):
+				return val
+	#print("Coordinate \'%s\': is blank" %(coor))
+	return None
+
+# Shift 'right' cell
+def coor_shift_right(ws, coor):
+	temp = list([coor['lastcol'] + 1, coor['firstrow']])
+	return find_cell(ws, temp)
+
+# Shift 'left' cell
+def coor_shift_left(ws, coor):
+	col = coor['firstcol'] - 1
+	if col < 1:
+		col = 1
+	temp = list([col, coor['firstrow']])
+	return find_cell(ws, temp)
+
+# Shift 'up' cell
+def coor_shift_up(ws, coor):
+	row = coor['firstrow'] - 1
+	if row < 1:
+		row = 1
+	temp = list([coor['firstcol'], row])
+	return find_cell(ws, temp)
+
+# Shift 'down' cell
+def coor_shift_down(ws, coor):
+	temp = list([coor['firstcol'], coor['lastrow'] + 1])
+	return find_cell(ws, temp)
+
+# Get test case row
+def row_of_testcase(ws, symbol):
+	cur_cell = find_cell(ws, symbol)
+	first_row_of_tc = cur_cell['lastrow'] + 1
+	while (get_cell_value(ws, cur_cell) != None):
+		cur_cell = coor_shift_down(ws, cur_cell)
+	last_row_of_tc = cur_cell['firstrow'] - 1
+	return first_row_of_tc, last_row_of_tc
