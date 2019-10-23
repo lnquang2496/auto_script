2f854b040e87eb0d9bacbbd359d2b83bd690892a
QuangLa <quang.la.gx@renesas.com>
2019-10-23 14:23:59 +0700

    Update script

---
+++ myproject.bat
@@ -0,0 +1,10 @@
+@echo off
+call cls
+call set PCL_PATH=D:\auto_script_asil\auto_script
+call set PCL_NAME=2.1_IMR_OSAL_CTRL_PCL.xlsx
+if exist output del /q output
+
+call python myproject.py --idir %PCL_PATH% --ifile %PCL_NAME% --wsheet r_imr_osal_ctl_start --check_seq True
+
+call set CAN_WP=C:\Users\quangla\workspace\test_imr_ut\Cantata\tests
+copy test_r_imr_osal_ctl_start.h %CAN_WP%\test_r_imr_osal_ctl\test_r_imr_osal_ctl_start.h
